e9f4b5df7b1a84873eaadaddd884a761f4de4a82
Om Dhawan <106106159+Om-Dhawan@users.noreply.github.com>
2023-03-19 00:34:27 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,163 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+from email.message import EmailMessage
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    r.non_speaking_duration=0.2
+    r.energy_threshold=400
+    with sr.Microphone() as source:
+        speak("Listening to you sir")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)    
+        speak("Say that again please...")  
+        return "None"
+    return query
+
+# def sendEmail(to, content):
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.ehlo()
+#     server.starttls()
+#     server.login('youremail@gmail.com', 'your-password')
+#     server.sendmail('youremail@gmail.com', to, content)
+#     server.close()
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "Pikachu0304xd@gmail.com"
+    msg['from'] = user
+    password = "kismreeqtucjsdik"
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+    
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+            
+        elif 'who is' in command:
+            person = command.replace('who is', '')
+            info = wikipedia.summary(person, 2)
+            print(info)
+            talk(info)
+            done()
+            # talk("Task completed successfully, now what's next task for me?")
+            
+        elif 'what is' in command:
+            thing = command.replace('what is', '')
+            info = wikipedia.summary(thing, 2)
+            print(info)
+            talk(info)
+            done()
+            # talk("Task completed successfully, now what's next task for me?")
+        
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime("%H:%M:%S")
+            talk('Sir, Current time is ' + time)
+            done()
+            # talk("Task completed successfully, now what's next task for me?")
+            
+        elif 'are you single' in command:
+            talk('I am in a relationship with Alexa')
+            
+        elif 'joke' in command:
+            j=pyjokes.get_joke()
+            print(j)
+            talk(j)
+            done()
+            # talk("Task completed successfully, now what's next task for me?")
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+        elif 'play music' in query:
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\omdhavan\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "omdhawan02@gmail.com"    
+                email_alert("Jarvis", content,to)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend. I am not able to send this email")   
+                
+        elif 'bye' in query:
+            speak("Good Bye Sir!")
+            break 
